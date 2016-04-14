e4522fd8bbf3f5445b9e24cfb2f857c35ab6ce55
theJPster <github@thejpster.org.uk>
2016-04-14 19:50:56 +0000

    Added build shell script which does the objcopy. I wish cargo would do this for me.

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cargo build --target=thumbv7em-none-eabi
+echo "Converting elf -> bin"
+arm-none-eabi-objcopy -O binary ./target/thumbv7em-none-eabi/debug/bare-metal-arm-rust ./target/thumbv7em-none-eabi/debug/bare-metal-arm-rust.bin
+ls -l ./target/thumbv7em-none-eabi/debug/bare-metal-arm-rust*
