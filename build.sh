60c0bc2405ce29f99a1a02f9127787020e578e74
Jonathan 'theJPster' Pallant <git@thejpster.org.uk>
2016-04-23 12:17:53 +0000

    Build script bails out early.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 cargo build --target=thumbv7em-none-eabi
 echo "Converting elf -> bin"
 arm-none-eabi-objcopy -O binary ./target/thumbv7em-none-eabi/debug/bare-metal-arm-rust ./target/thumbv7em-none-eabi/debug/bare-metal-arm-rust.bin
