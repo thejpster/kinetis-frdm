9c83d1ef3b508c319f57f3c9ade9928e6792fd09
theJPster <github@thejpster.org.uk>
2016-08-07 19:46:09 +0100

    Moved primer into libs folder. Set environment variables for xargo build.

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
-PRIMER_TARGET=lm4f120
-xargo build --target=$PRIMER_TARGET
+AR_lm4f120=arm-none-eabi-ar CC_lm4f120=arm-none-eabi-gcc xargo build --target=lm4f120
 echo "Converting elf -> bin"
 arm-none-eabi-objcopy -O binary ./target/$PRIMER_TARGET/debug/bare-metal-arm-rust ./target/$PRIMER_TARGET/debug/bare-metal-arm-rust.bin
 ls -l ./target/$PRIMER_TARGET/debug/bare-metal-arm-rust*
