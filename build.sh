f05a0140b9cd0d7b65ab5450c242f2b4289eab2e
theJPster <github@thejpster.org.uk>
2016-08-07 21:57:47 +0100

    Added xargo to the path when on travis.

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 set -e
 PRIMER_TARGET=lm4f120
+if ! which xargo;
+then
+	echo "Xargo not in path. Is this travis?"
+	if [ -d /home/travis/.cargo/bin ];
+	then
+		echo "Yes - setting path for travis"
+		export /home/travis/.cargo/bin:$PATH
+	else
+		echo "Xargo not found"
+		exit 1
+	fi
+fi
 xargo build --target=$PRIMER_TARGET -v
 echo "Converting elf -> bin"
 arm-none-eabi-objcopy -O binary ./target/$PRIMER_TARGET/debug/bare-metal-arm-rust ./target/$PRIMER_TARGET/debug/bare-metal-arm-rust.bin
