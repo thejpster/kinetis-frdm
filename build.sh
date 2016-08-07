4f1b793e3369e9bf0e686445be16818a6697453f
theJPster <github@thejpster.org.uk>
2016-08-07 19:56:30 +0100

    Don't appear to need magic environment variables now.

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
-AR_lm4f120=arm-none-eabi-ar CC_lm4f120=arm-none-eabi-gcc xargo build --target=lm4f120
+PRIMER_TARGET=lm4f120
+xargo build --target=$PRIMER_TARGET -v
 echo "Converting elf -> bin"
 arm-none-eabi-objcopy -O binary ./target/$PRIMER_TARGET/debug/bare-metal-arm-rust ./target/$PRIMER_TARGET/debug/bare-metal-arm-rust.bin
 ls -l ./target/$PRIMER_TARGET/debug/bare-metal-arm-rust*
