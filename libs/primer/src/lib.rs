8bf211d6af7f63e8baa6091566349b025d1ddbf6
theJPster <github@thejpster.org.uk>
2016-08-08 18:54:47 +0100

    Revert "Trying to use compiler-rt.rs. Doesn't currently build."
    This reverts commit 6cab4cacd6e382254e451676a14083a95007a7a3.

--- libs/primer/src/lib.rs
+++ libs/primer/src/lib.rs
@@ -117,6 +117,17 @@ pub extern "C" fn _Unwind_Resume() -> ! {
 #[lang="eh_personality"]
 extern "C" fn eh_personality() {}
 
+// Seemingly required by LLVM
+#[no_mangle]
+pub unsafe extern "C" fn __aeabi_memclr4(s: *mut u8, n: usize) -> *mut u8 {
+    rlibc::memset(s, 0, n)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn __aeabi_memmove(dest: *mut u8, src: *mut u8, n: usize) {
+    rlibc::memmove(dest, src, n);
+}
+
 /// Entry point of panic from the libcore crate.
 ///
 /// Required by the compiler.
