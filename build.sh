4dd92481d2ef65518f5d3035d05cbd708ec41619
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2016-10-16 21:52:43 +0100

    Build debug, release and docs automatically.

--- build.sh
+++ build.sh
@@ -7,23 +7,20 @@ set -e
 # Copyright (c) 2016 Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
 #
 
-MODE=debug
-#ARGS=-v for verbose mode
-ARGS=
+echo "Running xargo debug..."
+xargo build --example launchpad_blink
 
-if [ "$1" == "--release" ];
-then
-    MODE=release
-    ARGS=--release
-fi
+echo "Running xargo release..."
+xargo build --release --example launchpad_blink
 
-echo "Running xargo..."
-xargo build $ARGS --example launchpad_blink
+echo "Running xargo docs..."
+xargo doc
 
 echo "Converting elf -> bin..."
-arm-none-eabi-objcopy -O binary ./target/thumbv7em-none-eabihf/$MODE/examples/launchpad_blink ./target/thumbv7em-none-eabihf/$MODE/examples/launchpad_blink.bin
+arm-none-eabi-objcopy -O binary ./target/thumbv7em-none-eabihf/debug/examples/launchpad_blink ./target/thumbv7em-none-eabihf/debug/examples/launchpad_blink.bin
+arm-none-eabi-objcopy -O binary ./target/thumbv7em-none-eabihf/release/examples/launchpad_blink ./target/thumbv7em-none-eabihf/release/examples/launchpad_blink.bin
 
 echo "Examples available..."
-ls -lh ./target/thumbv7em-none-eabihf/$MODE/examples/*
+ls -lh ./target/thumbv7em-none-eabihf/*/examples/*
 
 echo "Done!"
